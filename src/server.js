f227c28b9cd4045f117827ebf4c671beb2571f2c
ranierecosta <raniere_@live.com>
2019-03-07 13:54:08 -0300

    Configuração do mongoose - conexão com mongodb + docker

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    // MongoDB hospedado -> mongodb://usuario:senha@localhost:27017/database
+    // Docker -> mongodb://localhost:27017/database
+
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
